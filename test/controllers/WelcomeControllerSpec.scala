bdb2a9508d7791c356b79e248de0a477bb6de6ed
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-04-10 08:15:03 +0100

    added initial tests and code to pass tests re 'welcome'

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -3,14 +3,35 @@ package controllers
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.FakeRequest
+import play.api.test.Helpers.{status,_}
 
 class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest{
 
   "WelcomeController GET" should {
     "return a successful response" in {
-      val controller = new WelcomeControllerSpec
+      val controller = new WelcomeController
       val result = controller.welcome.apply(FakeRequest())
       status(result) mustBe OK
+    }
+
+    "respond to the /welcome url" in {
+      val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
+      val home = route(app, request).get
+      status(home) mustBe OK
+    }
+
+    "return some html" in {
+
+      val controller = new WelcomeController
+      val result = controller.welcome.apply(FakeRequest())
+      contentType(result) mustBe Some("text/html")
+    }
+
+    "say hello and have a title" in {
+      val controller = new WelcomeController
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      contentAsString(result) must include("<h1>Hello!</h1>")
+      contentAsString(result) must include("<title>Welcome</title>")
     }
   }
 
